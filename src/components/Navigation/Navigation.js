9abc3b5c88ae11ad5b8afa4a3ee55341277b43c8
Roy Lulavy <49700286+roeilulavy@users.noreply.github.com>
2022-04-22 16:13:14 +0300

    Nav edit

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,12 +1,23 @@
-import { useNavigation } from 'react-router';
+import { useNavigate } from 'react-router';
 import './Navigation.css';
 
-export function Navigation () {
-
-
+export function Navigation ({ isLoggedIn }) {
+  const navigation = useNavigate()
 
   return (
-    <>
-    </>
+    <nav className='navigation'>
+
+      <h1 className='navigation__title'>NewsExplorer</h1>
+
+      <ul className='navigation__list'>
+        <li className='navigation__link'>
+          Home
+        </li>
+        <li className='navigation__button'>
+          Sign in
+        </li>
+      </ul>
+
+    </nav>
   )
 }
